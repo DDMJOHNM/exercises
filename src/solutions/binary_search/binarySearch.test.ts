e973311cc9444daff43f6f297345b5eb6935ec87
John Mason <johnmason@Johns-iMac.local>
2026-03-09 14:40:53 +1300

    binary search

--- src/solutions/binary_search/binarySearch.test.ts
+++ src/solutions/binary_search/binarySearch.test.ts
@@ -2,7 +2,7 @@ import { binarySearch } from './binarySearch';
 
 describe('binarySearch', () => {
     test('should return 0 for [-1,0,2,4,6,8] and target 4', () => {
-        expect(binarySearch([-1,0,2,4,6,8],4)).toBe(4);
+        expect(binarySearch([-1,0,2,4,6,8],4)).toBe(3);
     });
 5
     test('should return -1 for [-1,0,2,4,6,8] and target 3', () => {
